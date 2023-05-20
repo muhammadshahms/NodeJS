86105e0f1da593d7d6ee7d5924682dc8e859abdc
Shah <muhammadshah516@gmail.com>
2023-05-20 15:32:04 +0500

    .

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,61 @@
 const express = require('express');
-
+const Users = require('../models/users');
+const multer = require('multer');
 const router = express.Router();
-router.get("/",(req,res)=>{
-    res.render('index',{title: 'HomePage'})
-})
-router.get("/add_user",(req,res)=>{
-    res.render('add_user',{title: 'AddUser'})
-})
+const fs = require('fs');
+const path = require('path');
+// Image upload configuration
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var uploadDir = "./uploads";
+    // Create the uploads directory if it doesn't exist
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    var fileExtension = file.originalname.split(".").pop();
+    var filename =
+      file.fieldname + "_" + Date.now() + "_" + file.originalname;
+    cb(null, filename.replace("." + fileExtension, "") + "." + fileExtension);
+  }
+});
 
+var upload = multer({
+  storage: storage
+}).single("image");
 
+// Route for adding a user
+router.post('/add', upload, async (req, res) => {
+  const user = new Users({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    image: req.file.filename
+  });
 
-module.exports=router;
+  try {
+    await user.save();
+    req.session.message = {
+      type: 'success',
+      message: 'User added successfully',
+    };
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Home page route
+router.get("/", (req, res) => {
+  res.render('index', { title: 'HomePage' });
+});
+
+// Add user page route
+router.get("/add_user", (req, res) => {
+  res.render('add_user', { title: 'AddUser' });
+});
+
+module.exports = router;
